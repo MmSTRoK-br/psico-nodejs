3f025453e8d82b1ef82433d606b4da950a563dee
qualityseg <128048171+qualityseg@users.noreply.github.com>
2023-08-08 15:42:31 -0300

    Update server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const pool = mysql.createPool({
 });
 
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://fair-ruby-caterpillar-wig.cyclic.app'],
+  origin: ['http://localhost:3000', 'https://fair-ruby-caterpillar-wig.cyclic.app', 'https://psico-painel.vercel.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
