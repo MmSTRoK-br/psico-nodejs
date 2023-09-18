b43a744cd33fcaa25c099a8c05dd874f7cf7af13
qualityseg <128048171+qualityseg@users.noreply.github.com>
2023-09-18 11:15:42 -0300

    Update server.js

--- server.js
+++ server.js
@@ -57,14 +57,14 @@ app.get('/checkAvaliacao', async (req, res) => {
 
 // Exemplo de rota no Node.js
 app.get('/api/evaluations/count', async (req, res) => {
-  const institutionNome = req.query.institutionName;
+  const institutionNome = req.query.institutionNome;
   
   try {
     // Consulta para contar todas as avaliações
     const totalEvaluations = await db.query('SELECT COUNT(*) FROM avaliacoes_realizadas WHERE instituicaoNome = ? AND avaliacao_realizada = 1', [institutionNome]);
 
     // Consulta para contar as avaliações feitas hoje
-    const evaluationsToday = await db.query('SELECT COUNT(*) FROM avaliacoes_realizadas WHERE instituicaoNome = ? AND avaliacao_realizada = 1 AND DATE(created_at) = CURDATE()', [institutionName]);
+    const evaluationsToday = await db.query('SELECT COUNT(*) FROM avaliacoes_realizadas WHERE instituicaoNome = ? AND avaliacao_realizada = 1 AND DATE(created_at) = CURDATE()', [institutionNome]);
 
     res.json({ total: totalEvaluations[0]['COUNT(*)'], today: evaluationsToday[0]['COUNT(*)'] });
   } catch (error) {
